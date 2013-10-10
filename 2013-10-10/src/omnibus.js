032be1ed0339558cc7a6bf6180247d836e05a6ad
Arnaud Bailly <abailly@murex.com>
2013-10-10 13:43:42 +0200

    omnibus goes down after reaching 5th floor

--- 2013-10-10/src/omnibus.js
+++ 2013-10-10/src/omnibus.js
@@ -1,10 +1,15 @@
 var Omnibus = (function () {
-    var commands = ["OPEN", "CLOSE","UP"];
+    var commands = ["OPEN", "CLOSE","UP",
+        "OPEN", "CLOSE","UP",
+        "OPEN", "CLOSE","UP",
+        "OPEN", "CLOSE","UP",
+        "OPEN", "CLOSE","UP",
+        "OPEN", "CLOSE","DOWN"];
     var next = 0;
     
 
     var nextCommand = function () {
-        return commands[next++ % 3];
+        return commands[next++ % commands.length];
     }
     return {
         nextCommand : nextCommand
--- 2013-10-10/test/omnibus_test.js
+++ 2013-10-10/test/omnibus_test.js
@@ -3,46 +3,45 @@ var vows = require("vows"),
     assert = require('assert');
 
 
-
 vows.describe("controlleur de l'omnibus").addBatch({
-    'when omnibus is asked for nextCommand':  {
-        topic: function() {
+    'when omnibus is asked for nextCommand': {
+        topic: function () {
             var omnibus = new Omnibus();
             return omnibus.nextCommand();
         },
-        
-        'it opens the door': function(topic) {
-            assert.equal(topic,"OPEN");    
-        }
-    }, 
-    
-    'when omnibus is asked for nextCommand twice':  {
-        topic: function() {
-            var omnibus = new Omnibus();
-            omnibus.nextCommand();
-            return omnibus.nextCommand();
-        },
-        
-        'it opens then close the door': function(topic) {
-            assert.equal(topic,"CLOSE");    
+
+        'it opens the door': function (topic) {
+            assert.equal(topic, "OPEN");
         }
     },
 
-    'when omnibus is asked for nextCommand three times':  {
-        topic: function() {
+    'when omnibus is asked for nextCommand twice': {
+        topic: function () {
             var omnibus = new Omnibus();
-            omnibus.nextCommand();
             omnibus.nextCommand();
             return omnibus.nextCommand();
         },
 
-        'it ends up going up': function(topic) {
-            assert.equal(topic,"UP");
+        'it opens then close the door': function (topic) {
+            assert.equal(topic, "CLOSE");
         }
     },
 
-    'when omnibus is asked for nextCommand four times':  {
-        topic: function() {
+    'when omnibus is asked for nextCommand three times': {
+        topic: function () {
+            var omnibus = new Omnibus();
+            omnibus.nextCommand();
+            omnibus.nextCommand();
+            return omnibus.nextCommand();
+        },
+
+        'it ends up going up': function (topic) {
+            assert.equal(topic, "UP");
+        }
+    },
+
+    'when omnibus is asked for nextCommand four times': {
+        topic: function () {
             var omnibus = new Omnibus();
             omnibus.nextCommand();
             omnibus.nextCommand();
@@ -50,9 +49,23 @@ vows.describe("controlleur de l'omnibus").addBatch({
             return omnibus.nextCommand();
         },
 
-        'it opens the door': function(topic) {
-            assert.equal(topic,"OPEN");
+        'it opens the door': function (topic) {
+            assert.equal(topic, "OPEN");
+        }
+    },
+
+    'when omnibus reaches fiveth floor': {
+        topic: function () {
+            var omnibus = new Omnibus();
+            for (var i = 0; i < 17; i++) {
+                omnibus.nextCommand();
+            }
+            return omnibus.nextCommand();
+        },
+
+        'it goes down': function (topic) {
+            assert.equal(topic, "DOWN");
         }
     }
-    
+
 }).run();
