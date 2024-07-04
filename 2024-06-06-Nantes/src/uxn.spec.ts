f566da84e62b4b670dd0019679f095d40f0b10d6
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-07-04 18:52:18 +0200

    Implement DEI for console

--- 2024-06-06-Nantes/src/uxn.spec.ts
+++ 2024-06-06-Nantes/src/uxn.spec.ts
@@ -1,37 +1,49 @@
 import { VM, VMDevice } from "./vm";
 
 describe("when", () => {
-  [0x12, 0x13].forEach((value) =>
-    it(`LIT should ${value} next byte in stack`, () => {
-      const vm = new VM();
-      const program = `\x80${String.fromCodePoint(value)}`;
-      vm.execute(program);
+    [0x12, 0x13].forEach((value) =>
+        it(`LIT should ${value} next byte in stack`, () => {
+            const vm = new VM();
+            const program = `\x80${String.fromCodePoint(value)}`;
+            vm.execute(program);
 
-      expect(vm.pop()).toBe(value);
-    })
-  );
+            expect(vm.pop()).toBe(value);
+        })
+    );
 
-  it(`Two LIT should push two bytes in stack`, () => {
-    const vm = new VM();
-    const program = `\x80\x12\x80\x13`;
-    vm.execute(program);
+    it(`Two LIT should push two bytes in stack`, () => {
+        const vm = new VM();
+        const program = `\x80\x12\x80\x13`;
+        vm.execute(program);
 
-    expect(vm.pop()).toBe(0x13);
-    expect(vm.pop()).toBe(0x12);
-  });
-  it(`POP should pop one byte from stack`, () => {
-    const vm = new VM();
-    const program = `\x80\x12\x80\x13\x02`;
-    vm.execute(program);
+        expect(vm.pop()).toBe(0x13);
+        expect(vm.pop()).toBe(0x12);
+    });
 
-    expect(vm.pop()).toBe(0x12);
-  });
-  it(`DEO should write one byte to device`, () => {
-    const device = new VMDevice();
-    const vm = new VM(device);
-    const program = `\x80\x12\x80\x18\x17`;
-    vm.execute(program);
+    it(`POP should pop one byte from stack`, () => {
+        const vm = new VM();
+        const program = `\x80\x12\x80\x13\x02`;
+        vm.execute(program);
 
-    expect(device.get(0x08)).toEqual([0x12]);
-  });
+        expect(vm.pop()).toBe(0x12);
+    });
+
+    it(`DEO should write one byte to device`, () => {
+        const device = new VMDevice();
+        const vm = new VM(device);
+        const program = `\x80\x12\x80\x18\x17`;
+        vm.execute(program);
+
+        expect(device.get(0x08)).toEqual([0x12]);
+    });
+
+    it(`DEI should read one byte from device`, () => {
+        const device = new VMDevice();
+        device.in = [0x42];
+        const vm = new VM(device);
+        const program = `\x80\x12\x16`;
+        vm.execute(program);
+
+        expect(vm.pop()).toEqual(0x42);
+    });
 });
--- 2024-06-06-Nantes/src/vm.ts
+++ 2024-06-06-Nantes/src/vm.ts
@@ -1,44 +1,55 @@
 export class VM {
-  stack: number[] = [];
-  device: VMDevice;
-  execute(program: string) {
-    for (var i = 0; i < program.length; i++) {
-      switch (program.charCodeAt(i)) {
-        case 0x80:
-          this.stack.push(program.charCodeAt(++i));
-          break;
-        case 0x02:
-          this.stack.pop();
-          break;
-        case 0x17:
-          const port = this.stack.pop() ?? 0;
-          const value = this.stack.pop() ?? 0;
-          this.device.output(port, value);
-          break;
-      }
+    stack: number[] = [];
+    device: VMDevice;
+    execute(program: string) {
+        for (var i = 0; i < program.length; i++) {
+            switch (program.charCodeAt(i)) {
+                case 0x80:
+                    this.stack.push(program.charCodeAt(++i));
+                    break;
+                case 0x02:
+                    this.stack.pop();
+                    break;
+                case 0x16: {
+                    const port = this.stack.pop() ?? 0;
+                    this.stack.push(this.device.input(port));
+                    break;
+                }
+                case 0x17: {
+                    const port = this.stack.pop() ?? 0;
+                    const value = this.stack.pop() ?? 0;
+                    this.device.output(port, value);
+                    break;
+                }
+            }
+        }
     }
-  }
-  pop(): number | undefined {
-    return this.stack.pop();
-  }
+    pop(): number | undefined {
+        return this.stack.pop();
+    }
 
-  constructor(device: VMDevice | null = null) {
-    this.device = device ?? new VMDevice();
-  }
+    constructor(device: VMDevice | null = null) {
+        this.device = device ?? new VMDevice();
+    }
 }
 
 export class VMDevice {
-  out: number[] = [];
-  output(port: number, value: number) {
-    this.out.push(value);
-  }
-  get(port: number): number[] {
-    return this.out;
-  }
+    out: number[] = [];
+    in: number[] = [];
+
+    output(port: number, value: number) {
+        this.out.push(value);
+    }
+    input(port: number): number {
+        return this.in.pop() ?? 0;
+    }
+    get(port: number): number[] {
+        return this.out;
+    }
 }
 
 export class Console extends VMDevice {
-    output (port: number, value : number) {
+    output(port: number, value: number) {
         console.log(`${String.fromCharCode(value)}`);
     }
 }
